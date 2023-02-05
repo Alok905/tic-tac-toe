cdd140fb91f60b108c29c65c0bd6ac7a528dc350
Alok905 <alokranjanjoshi07567@gmail.com>
2023-02-05 22:22:07 +0530

    user input X and O added

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,23 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Square from "./Square";
 
 const Board = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIisXNext] = useState(false);
+  const handleSquareClick = (position) => {
+    setBoard(
+      board.map((val, pos) => {
+        if (pos == position) return isXNext ? "X" : "O";
+        return val;
+      })
+    );
+    setIisXNext((prev) => !prev);
+  };
+
+  const renderSquare = (position) => (
+    <Square
+      value={board[position]}
+      onClick={() => handleSquareClick(position)}
+    />
+  );
+
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={1} />
-        <Square value={2} />
-        <Square value={3} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        <Square value={4} />
-        <Square value={5} />
-        <Square value={6} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Square value={7} />
-        <Square value={8} />
-        <Square value={9} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
